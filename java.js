6b784ed975eea9f77c04e62d774229888a5dbd05
ericpzl <ericperezleon3@gmail.com>
2024-03-03 22:49:41 +0000

    proyecto terminado2

--- java.js
+++ java.js
@@ -9,3 +9,29 @@ function closeMenu() {
 }
 
 /* este menu hamburguesa fue hecho con bastante ayuda porque a la hora de intentarlo daba muchos fallos */ 
+
+function validateForm() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var errorMessage = document.getElementById("error-message");
+
+  errorMessage.innerHTML = ""; 
+
+  if (name === "" || email === "" || message === "") {
+      errorMessage.innerHTML = "Por favor, completa todos los campos.";
+      return false;
+  }
+
+ 
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+      errorMessage.innerHTML = "Por favor, introduce un correo electrónico válido.";
+      return false;
+  }
+
+
+
+  alert("Formulario enviado correctamente.");
+  return true;
+}
