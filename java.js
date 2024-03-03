8b60a70a9669bfa2654ce36dfe08973a617301b8
ericpzl <ericperezleon3@gmail.com>
2024-03-03 22:28:46 +0000

    Entrega final

---
+++ java.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+  var navList = document.querySelector('.nav-list');
+  navList.classList.toggle('show');
+}
+
+function closeMenu() {
+  var navList = document.querySelector('.nav-list');
+  navList.classList.remove('show');
+}
+
+/* este menu hamburguesa fue hecho con bastante ayuda porque a la hora de intentarlo daba muchos fallos */ 
